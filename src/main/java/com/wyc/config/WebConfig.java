33422eae37e6768bf9e7bcfcc82c62fabf8fd19a
王煜川 <=>
2015-06-16 14:04:10 +0800

    添加微信借口测试用例

--- src/main/java/com/wyc/config/WebConfig.java
+++ src/main/java/com/wyc/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.wyc.config;
-
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
+
+@EnableWebMvc
 @Configuration
 @ComponentScan(basePackages="com.wyc",
 				excludeFilters={
 					@Filter(type=FilterType.ASSIGNABLE_TYPE,value=AppConfig.class)
 })
-public class WebConfig{
-
+public class WebConfig extends WebMvcConfigurerAdapter{
+	
 }
--- src/main/java/com/wyc/controller/api/BaseApi.java
+++ src/main/java/com/wyc/controller/api/BaseApi.java
@@ -1,20 +1,46 @@
 package com.wyc.controller.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseApi {
-		@RequestMapping(value = "/api/test")
-		public void test(@RequestParam(value="signature")String signature,
-				@RequestParam(value="timestamp")String timestamp,
-				@RequestParam(value="nonce")String nonce,
-				@RequestParam(value="echostr")String echostr){
-			System.out.println(signature);
-			System.out.println(timestamp);
-			System.out.println(nonce);
-			System.out.println(echostr);
-			
+	private String token = "onlineRetailers";
+
+	@RequestMapping(value = "/api/test")
+	public String test(@RequestParam(value = "signature") String signature,
+			@RequestParam(value = "timestamp") String timestamp,
+			@RequestParam(value = "nonce") String nonce,
+			@RequestParam(value = "echostr") String echostr) {
+		System.out.println(signature);
+		System.out.println(timestamp);
+		System.out.println(nonce);
+		System.out.println(echostr);
+		return echostr;
+
+	}
+
+	private static String byteToStr(byte[] byteArray) {
+		String strDigest = "";
+		for (int i = 0; i < byteArray.length; i++) {
+			strDigest += byteToHexStr(byteArray[i]);
 		}
+		return strDigest;
+	}
+
+	private static String byteToHexStr(byte mByte) {
+		char[] Digit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+				'B', 'C', 'D', 'E', 'F' };
+		char[] tempArr = new char[2];
+		tempArr[0] = Digit[(mByte >>> 4) & 0X0F];
+		tempArr[1] = Digit[mByte & 0X0F];
+
+		String s = new String(tempArr);
+		return s;
+	}
+
 }
