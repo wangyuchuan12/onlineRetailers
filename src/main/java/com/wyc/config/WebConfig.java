de719afc3c332de7ecfd633dd085cf4cc9c9816f
王煜川 <=>
2015-06-16 14:39:49 +0800

    测试

--- src/main/java/com/wyc/config/WebConfig.java
+++ src/main/java/com/wyc/config/WebConfig.java
@@ -1,12 +1,15 @@
 package com.wyc.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
@@ -15,5 +18,27 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 					@Filter(type=FilterType.ASSIGNABLE_TYPE,value=AppConfig.class)
 })
 public class WebConfig extends WebMvcConfigurerAdapter{
-	
+	 	@Bean
+	    public InternalResourceViewResolver viewResolver() {
+	        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+	        viewResolver.setPrefix("/WEB-INF/views/");
+	        viewResolver.setSuffix(".jsp");
+	        return viewResolver;
+	    }
+
+	    @Bean
+	    public DispatcherServlet dispatcherServlet() {
+	        return new DispatcherServlet();
+	    }
+
+	    @Override
+	    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+	        configurer.enable();
+	    }
+
+	    @Override
+	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+	        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+	    }
 }
