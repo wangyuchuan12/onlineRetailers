6a49af015c5864717bc8e2bf47b94370c74a32a3
王煜川 <=>
2015-06-17 09:32:32 +0800

    no message

--- src/main/java/com/wyc/controller/api/BaseApi.java
+++ src/main/java/com/wyc/controller/api/BaseApi.java
@@ -5,26 +5,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
+import org.jdom.Document;
+import org.jdom.input.SAXBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.helpers.DefaultHandler;
 
 @RestController
 public class BaseApi {
 	private String token = "onlineRetailers";
 
 	@RequestMapping(value = "/api/test")
-	public String test(HttpServletRequest servletRequest) {
+	public String test(HttpServletRequest servletRequest)throws Exception {
 		String signature = servletRequest.getParameter("signature");
 		String timestamp = servletRequest.getParameter("timestamp");
 		String nonce = servletRequest.getParameter("nonce");
 		String echostr = servletRequest.getParameter("echostr");
-		System.out.println("ceshi");
+		System.out.println("...........................get........................................");
 		System.out.println(signature);
 		System.out.println(timestamp);
 		System.out.println(nonce);
 		System.out.println(echostr);
+		System.out.println("............................post........................................");
+		
+		SAXBuilder saxBuilder = new SAXBuilder();
+		Document document = saxBuilder.build(servletRequest.getInputStream());
+		System.out.println(document.toString());
 		return echostr;
 
 	}
