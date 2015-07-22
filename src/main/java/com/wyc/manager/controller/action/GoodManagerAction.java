556b15891dba98d77e960d5edbb2fb653d56dd1f
王煜川 <=>
2015-07-22 17:36:29 +0800

    日志打印

--- src/main/java/com/wyc/manager/controller/action/GoodManagerAction.java
+++ src/main/java/com/wyc/manager/controller/action/GoodManagerAction.java
@@ -1,11 +1,17 @@
 package com.wyc.manager.controller.action;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.wyc.config.AppConfig;
+
 @Controller
 public class GoodManagerAction {
+    final static Logger logger = LoggerFactory.getLogger(GoodManagerAction.class);
     public GoodManagerAction() {
+        logger.debug("...........................GoodManagerAction");
         System.out.println("...........................GoodManagerAction");
     }
     @RequestMapping("/manager/good_add")
