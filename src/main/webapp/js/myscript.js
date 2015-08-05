c006f28459e1979a5a1c59946824e1c1e1299ce1
王煜川 <=>
2015-08-05 17:49:50 +0800

    测试

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -19,7 +19,6 @@ function goodItemOnClick(id,token){
 }
 
 function toPayOnClick(goodId , payType){
-	var url;
 	alert(window.localStorage.getItem("userToken"));
 	if(window.localStorage.getItem("userToken")){
 		url = webPath+"/info/good_info_pay?pay_type="+payType+"&good_id="+goodId+"&token="+window.localStorage.getItem("userToken");
@@ -27,6 +26,7 @@ function toPayOnClick(goodId , payType){
 		url = "https://open.weixin.qq.com/connect/oauth2/authorize?" +
 		"appid=wx7e3ed2dc655c0145&redirect_uri="+webPath+"/info/good_info_pay?good_id="+goodId+"&pay_type="+payType+"&response_type=code&scope=snsapi_userinfo&state=123&connect_redirect=1#wechat_redirect";
 	}
+	alert(url);
 	window.location.href=url;
 }
 
