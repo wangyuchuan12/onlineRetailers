ae643b4a348795674600428eb366059b4618d05b
王煜川 <=>
2015-08-20 16:15:02 +0800

    调试

--- src/main/java/com/wyc/service/CustomerService.java
+++ src/main/java/com/wyc/service/CustomerService.java
@@ -3,19 +3,22 @@ package com.wyc.service;
 import java.util.UUID;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.wyc.domain.Customer;
 import com.wyc.repositories.CustomerRepository;
 @Service
 public class CustomerService {
     @Autowired
     private CustomerRepository customerRepository;
+    final static Logger logger = LoggerFactory.getLogger(CustomerService.class);
     public Customer add(Customer customer){
         customer.setId(UUID.randomUUID().toString());
         customer.setCreateAt(new DateTime());
         customer.setUpdateAt(new DateTime());
+        logger.debug("add customer the id is {},the openid is {}",customer.getId(),customer.getOpenId());
         return customerRepository.save(customer);
     }
     
