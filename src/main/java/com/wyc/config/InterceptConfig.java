03e80f8e649773ffe636639efffaf4ebad4b4b7c
王煜川 <=>
2015-08-25 12:31:20 +0800

    如果别昵无法识别就把别昵替换为无法识别

--- src/main/java/com/wyc/config/InterceptConfig.java
+++ src/main/java/com/wyc/config/InterceptConfig.java
@@ -245,7 +245,7 @@ public class InterceptConfig {
                     logger.debug("save to database success ,the key is {} , the token is {}" , key , token);
                 } catch (Exception e) {
                     logger.error("get userInfo from wx has error");
-                    e.printStackTrace();
+                    userInfo.setNickname("无法识别");
                     //做最后一层保障，保证在数据库当中有userInfo
                     userInfo = wxUserInfoService.add(userInfo);
                 }
