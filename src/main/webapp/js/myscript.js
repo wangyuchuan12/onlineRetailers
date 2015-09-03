03cbd0187e941e2a992128a839710ae7c2c09d80
王煜川 <=>
2015-09-03 14:16:01 +0800

    测试

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -348,15 +348,15 @@ function footActive(id){
 
 
 
-function wxConfig(appId,signature,noncestr,timestamp){
+function wxConfig(appId,signature,noncestr,t){
 	alert(appId);
 	alert(signature);
 	alert(noncestr);
-	alert(timestamp);
+	alert(t);
 	wx.config({
 	    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 	    appId:appId, // 必填，公众号的唯一标识
-	    timestamp:timestamp, // 必填，生成签名的时间戳
+	    timestamp:t, // 必填，生成签名的时间戳
 	    nonceStr:noncestr, // 必填，生成签名的随机串
 	    signature: signature,// 必填，签名，见附录1
 	    jsApiList: ['onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
