d96045a1145e09a22b8cc896bdfc82df8c0f0596
王煜川 <=>
2015-09-03 14:06:22 +0800

    测试

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -352,7 +352,7 @@ function wxConfig(appId,signature,noncestr,timestamp){
 	alert(appId);
 	alert(signature);
 	alert(noncestr);
-	
+	alert(timestamp);
 	wx.config({
 	    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 	    appId:appId, // 必填，公众号的唯一标识
