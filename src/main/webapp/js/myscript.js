80f1ac38e7578631a5ecfa3b6a98232dce1aa698
王煜川 <=>
2015-09-09 16:19:42 +0800

    微信支付测试页面

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -382,8 +382,7 @@ function wxOnMenuShareAppMessage(title,desc,link,imgUrl,type,dataUrl){
 
 function onChooseWXPay(appid,pack,nonceStr,paySign,signType,timestamp){
 	wx.chooseWXPay({
-		appId:appid,
-		timeStamp:timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+		timestamp:timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
 	    nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
 	    package: pack, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
 	    signType:signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
