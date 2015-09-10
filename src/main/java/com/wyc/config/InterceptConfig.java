8959e43b3c80c649a008d2fe8702fb54bb52ed93
王煜川 <=>
2015-09-10 16:33:37 +0800

    测试

--- src/main/java/com/wyc/config/InterceptConfig.java
+++ src/main/java/com/wyc/config/InterceptConfig.java
@@ -367,7 +367,84 @@ public class InterceptConfig {
             myHttpServletRequest.setToken(token);
         }
         
-        
+        if(method.getAnnotation(WxChooseWxPay.class)!=null){
+           
+      
+            UserInfo userInfo = myHttpServletRequest.getUserInfo();
+            String openid = userInfo.getOpenid();
+            Request request = requestFactory.payUnifiedorder();
+            String appid = wxContext.getAppid();
+            String attach = "paytest";
+            String body = "JSAPI";
+            String mchId = wxContext.getMchId();
+            String nonceStr = "1add1a30ac87aa2db72f57a2375d8fec";
+            String notifyUrl = "http://www.chengxihome.com/api/test";
+            Calendar now = Calendar.getInstance();
+            String outTradeNo = now.get(Calendar.YEAR)
+                    +"-"+(now.get(Calendar.MONTH) + 1)
+                    +"-"+now.get(Calendar.DAY_OF_MONTH)
+                    +"-"+now.get(Calendar.HOUR_OF_DAY)
+                    +"-"+now.get(Calendar.MINUTE)
+                    +"-"+now.get(Calendar.SECOND)
+                    +"-"+new Random().nextInt(1000)+"";
+            String spbillCreateIp = httpServletRequest.getRemoteAddr();
+            String datetime = String.valueOf(System.currentTimeMillis() / 1000);
+            long totalFee = 1;
+            String tradeType = "JSAPI";
+            TreeMap<String, String> map = new TreeMap<String, String>();
+            map.put("openid", openid);
+            map.put("body", body);
+            map.put("out_trade_no", outTradeNo);
+            map.put("total_fee", totalFee+"");
+            map.put("notify_url", notifyUrl);
+            map.put("trade_type", tradeType);
+            map.put("appid", appid);
+            map.put("mch_id", mchId);
+            map.put("spbill_create_ip", spbillCreateIp);
+            map.put("nonce_str", nonceStr);
+            map.put("attach", attach);
+            String sign = MD5Util.createMd5Sign(map,wxContext.getKey()).toUpperCase();
+            StringBuffer sb2 = new StringBuffer();
+            sb2.append("<xml>");
+            sb2.append("<appid>"+appid+"</appid>");
+            sb2.append("<attach>"+attach+"</attach>");
+            sb2.append("<body>"+body+"</body>");
+            sb2.append("<mch_id>"+mchId+"</mch_id>");
+            sb2.append("<nonce_str>"+nonceStr+"</nonce_str>");
+            sb2.append("<notify_url>"+notifyUrl+"</notify_url>");
+            sb2.append("<openid>"+openid+"</openid>");
+            sb2.append("<out_trade_no>"+outTradeNo+"</out_trade_no>");
+            sb2.append("<spbill_create_ip>"+spbillCreateIp+"</spbill_create_ip>");
+            sb2.append("<total_fee>"+totalFee+"</total_fee>");
+            
+            sb2.append("<trade_type>"+tradeType+"</trade_type>");
+            sb2.append("<sign>"+sign+"</sign>");
+            sb2.append("</xml>");
+            logger.debug(sb2.toString());
+            Response response = request.post(sb2.toString());
+            SAXBuilder saxBuilder = new SAXBuilder();
+            Document document = saxBuilder.build(new StringReader(response.read()));
+            Element rootElement = document.getRootElement();
+            String prepayId = rootElement.getChildText("prepay_id");
+            datetime = String.valueOf(System.currentTimeMillis() / 1000);
+            SortedMap<String, String> map2  = new TreeMap<String, String>();
+            map2.put("appId", wxContext.getAppid());
+            map2.put("nonceStr", nonceStr);
+            map2.put("package", "prepay_id="+prepayId);
+            map2.put("signType", "MD5");
+            map2.put("timeStamp", datetime);
+            String paySign = MD5Util.createMd5Sign(map2,wxContext.getKey()).toUpperCase();
+            logger.debug("paySign is {}",paySign);
+            httpServletRequest.setAttribute("appId", wxContext.getAppid());
+            httpServletRequest.setAttribute("prepayId", prepayId);
+            httpServletRequest.setAttribute("package", "prepay_id="+prepayId);
+            httpServletRequest.setAttribute("nonceStr", nonceStr);
+            httpServletRequest.setAttribute("paySign", paySign);
+            httpServletRequest.setAttribute("signType", "MD5");
+            httpServletRequest.setAttribute("timestamp", datetime);
+            httpServletRequest.setAttribute("outTradeNo",outTradeNo);
+            logger.debug("prepayId is {}",prepayId);
+        }
         
         if(myHttpServletRequest!=null){
             args[0] = myHttpServletRequest;
@@ -378,86 +455,6 @@ public class InterceptConfig {
         try {
             Object url = proceedingJoinPoint.proceed(args);
             logger.debug("return url is {}",url);
-            
-            
-            if(method.getAnnotation(WxChooseWxPay.class)!=null){
-                
-                
-                UserInfo userInfo = myHttpServletRequest.getUserInfo();
-                String openid = userInfo.getOpenid();
-                Request request = requestFactory.payUnifiedorder();
-                String appid = wxContext.getAppid();
-                String attach = "paytest";
-                String body = "JSAPI";
-                String mchId = wxContext.getMchId();
-                String nonceStr = "1add1a30ac87aa2db72f57a2375d8fec";
-                String notifyUrl = "http://www.chengxihome.com/api/test";
-                Calendar now = Calendar.getInstance();
-                String outTradeNo = now.get(Calendar.YEAR)
-                        +"-"+(now.get(Calendar.MONTH) + 1)
-                        +"-"+now.get(Calendar.DAY_OF_MONTH)
-                        +"-"+now.get(Calendar.HOUR_OF_DAY)
-                        +"-"+now.get(Calendar.MINUTE)
-                        +"-"+now.get(Calendar.SECOND)
-                        +"-"+new Random().nextInt(1000)+"";
-                String spbillCreateIp = httpServletRequest.getRemoteAddr();
-                String datetime = String.valueOf(System.currentTimeMillis() / 1000);
-                Float totalFee = (Float)httpServletRequest.getAttribute("totalFree");
-                String tradeType = "JSAPI";
-                TreeMap<String, String> map = new TreeMap<String, String>();
-                map.put("openid", openid);
-                map.put("body", body);
-                map.put("out_trade_no", outTradeNo);
-                map.put("total_fee", totalFee+"");
-                map.put("notify_url", notifyUrl);
-                map.put("trade_type", tradeType);
-                map.put("appid", appid);
-                map.put("mch_id", mchId);
-                map.put("spbill_create_ip", spbillCreateIp);
-                map.put("nonce_str", nonceStr);
-                map.put("attach", attach);
-                String sign = MD5Util.createMd5Sign(map,wxContext.getKey()).toUpperCase();
-                StringBuffer sb2 = new StringBuffer();
-                sb2.append("<xml>");
-                sb2.append("<appid>"+appid+"</appid>");
-                sb2.append("<attach>"+attach+"</attach>");
-                sb2.append("<body>"+body+"</body>");
-                sb2.append("<mch_id>"+mchId+"</mch_id>");
-                sb2.append("<nonce_str>"+nonceStr+"</nonce_str>");
-                sb2.append("<notify_url>"+notifyUrl+"</notify_url>");
-                sb2.append("<openid>"+openid+"</openid>");
-                sb2.append("<out_trade_no>"+outTradeNo+"</out_trade_no>");
-                sb2.append("<spbill_create_ip>"+spbillCreateIp+"</spbill_create_ip>");
-                sb2.append("<total_fee>"+totalFee+"</total_fee>");
-                
-                sb2.append("<trade_type>"+tradeType+"</trade_type>");
-                sb2.append("<sign>"+sign+"</sign>");
-                sb2.append("</xml>");
-                logger.debug(sb2.toString());
-                Response response = request.post(sb2.toString());
-                SAXBuilder saxBuilder = new SAXBuilder();
-                Document document = saxBuilder.build(new StringReader(response.read()));
-                Element rootElement = document.getRootElement();
-                String prepayId = rootElement.getChildText("prepay_id");
-                datetime = String.valueOf(System.currentTimeMillis() / 1000);
-                SortedMap<String, String> map2  = new TreeMap<String, String>();
-                map2.put("appId", wxContext.getAppid());
-                map2.put("nonceStr", nonceStr);
-                map2.put("package", "prepay_id="+prepayId);
-                map2.put("signType", "MD5");
-                map2.put("timeStamp", datetime);
-                String paySign = MD5Util.createMd5Sign(map2,wxContext.getKey()).toUpperCase();
-                logger.debug("paySign is {}",paySign);
-                httpServletRequest.setAttribute("appId", wxContext.getAppid());
-                httpServletRequest.setAttribute("prepayId", prepayId);
-                httpServletRequest.setAttribute("package", "prepay_id="+prepayId);
-                httpServletRequest.setAttribute("nonceStr", nonceStr);
-                httpServletRequest.setAttribute("paySign", paySign);
-                httpServletRequest.setAttribute("signType", "MD5");
-                httpServletRequest.setAttribute("timestamp", datetime);
-                httpServletRequest.setAttribute("outTradeNo",outTradeNo);
-                logger.debug("prepayId is {}",prepayId);
-            }
             return url;
         } catch (Throwable e) {
             // TODO Auto-generated catch block
