b6ea847758898ed744c1f1981933ef3306881bc1
王煜川 <=>
2015-09-16 14:52:47 +0800

    测试

--- src/main/java/com/wyc/repositories/GoodGroupRepository.java
+++ src/main/java/com/wyc/repositories/GoodGroupRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import com.wyc.domain.GoodGroup;
 
 public interface GoodGroupRepository extends CrudRepository<GoodGroup, String>{
-    @Query("select g from com.wyc.domain.GoodGroup g where g.createAt = select max(createAt) from com.wyc.domain.GoodGroup")
+    @Query("select g from com.wyc.domain.GoodGroup g where g.createAt = (select max(createAt) from com.wyc.domain.GoodGroup)")
     public GoodGroup selectLastestGoodGroup();
 }
