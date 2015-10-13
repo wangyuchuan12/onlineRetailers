28a446b97672e10d67110fb64120ac638e407a9c
王煜川 <=>
2015-10-13 17:41:48 +0800

    记录当前页面状态补充

---
+++ src/main/java/com/wyc/annotation/NowPageRecordAnnotation.java
@@ -0,0 +1,15 @@
+package com.wyc.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NowPageRecordAnnotation {
+    //0表示商品列表，1表示商品详情，2表示购买页面
+    public int page();
+}
