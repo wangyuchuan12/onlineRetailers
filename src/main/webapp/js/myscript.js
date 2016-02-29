9eaae6014f87bb9a411a6b1bb6f1ddb31cf44f23
王煜川 <=>
2016-02-29 20:11:12 +0800

    获取团花费金额

--- src/main/webapp/js/myscript.js
+++ src/main/webapp/js/myscript.js
@@ -282,8 +282,8 @@ function skipToPersonCenter(token){
 	skipToUrl("/main/personal_center",token);
 }
 
-function skipToGoodPay(id,pay_type,token,groupId){
-	skipToUrl("/info/good_info_pay?good_id="+id+"&pay_type="+pay_type+"&group_id="+groupId,token);
+function skipToGoodPay(id,pay_type,token,groupId,cost){
+	skipToUrl("/info/good_info_pay?good_id="+id+"&pay_type="+pay_type+"&group_id="+groupId+"&cost="+cost,token);
 }
 
 function skipToPersonCenter(){
