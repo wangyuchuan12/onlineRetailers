97b265bb561bc730fac7d8e8326091d745497f78
王煜川 <=>
2016-03-25 09:56:40 +0800

    如果不是成功付款就不进行后续操作

--- src/main/java/com/wyc/controller/api/WxApi.java
+++ src/main/java/com/wyc/controller/api/WxApi.java
@@ -35,16 +35,18 @@ public class WxApi {
     @RequestMapping(value = "/api/wx/pay_success")
     public Object paySuccess(HttpServletRequest httpServletRequest)throws Exception{
         MyHttpServletRequest myHttpServletRequest = (MyHttpServletRequest)httpServletRequest;
+        PaySuccess paySuccess = (PaySuccess) myHttpServletRequest.getRequestObject(PaySuccess.class);
+        if(!paySuccess.getResultCode().equals("SUCCESS")){
+            return new StopToAfter();
+        }
+        
         String payType = myHttpServletRequest.getParameter("pay_type");
         if(payType!=null&&payType.equals("2")){
         	String outTradeNo = httpServletRequest.getParameter("outTradeNo");
         	httpServletRequest.setAttribute("outTradeNo", outTradeNo);
         	return null;
         }
-        PaySuccess paySuccess = (PaySuccess) myHttpServletRequest.getRequestObject(PaySuccess.class);
-        if(!paySuccess.getResultCode().equals("SUCCESS")){
-            return new StopToAfter();
-        }
+        
         String outTradeNo = paySuccess.getOutTradeNo();
        
         if(wxPaySuccessService.findByOutTradeNo(outTradeNo)!=null){
